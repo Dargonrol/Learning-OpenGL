5717d7c7d75d35e4dccbd8ba0b27384ee36e2a35
Dargonrol <dargonrol+github@posteo.com>
2025-12-28 03:54:44 +0100

    continued learning

--- src/main.cpp
+++ src/main.cpp
@@ -1,47 +1,20 @@
 #define GLAD_GL_IMPLEMENTATION
 #include <glad/gl.h>
 #define GLFW_INCLUDE_NONE
+#include <filesystem>
+#include <fstream>
 #include <GLFW/glfw3.h>
 
-#include "linmath.h"
-
-#include <stdlib.h>
-#include <stddef.h>
+#include <iostream>
 #include <stdio.h>
+#include <sstream>
 
-typedef struct Vertex
-{
-    vec2 pos;
-    vec3 col;
-} Vertex;
+#ifndef SOURCE_DIR
+#define SOURCE_DIR
+#endif
 
-static const Vertex vertices[3] =
-{
-    { { -0.6f, -0.4f }, { 1.f, 0.f, 0.f } },
-    { {  0.6f, -0.4f }, { 0.f, 1.f, 0.f } },
-    { {   0.f,  0.6f }, { 0.f, 0.f, 1.f } }
-};
+#define BASE_PATH std::filesystem::path(SOURCE_DIR)
 
-static const char* vertex_shader_text =
-"#version 330\n"
-"uniform mat4 MVP;\n"
-"in vec3 vCol;\n"
-"in vec2 vPos;\n"
-"out vec3 color;\n"
-"void main()\n"
-"{\n"
-"    gl_Position = MVP * vec4(vPos, 0.0, 1.0);\n"
-"    color = vCol;\n"
-"}\n";
-
-static const char* fragment_shader_text =
-"#version 330\n"
-"in vec3 color;\n"
-"out vec4 fragment;\n"
-"void main()\n"
-"{\n"
-"    fragment = vec4(color, 1.0);\n"
-"}\n";
 
 static void error_callback(int error, const char* description)
 {
@@ -52,6 +25,121 @@ static void key_callback(GLFWwindow* window, int key, int scancode, int action, 
 {
     if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
         glfwSetWindowShouldClose(window, GLFW_TRUE);
+}
+
+static void GLClearError()
+{
+    while (glGetError() != GL_NO_ERROR);
+}
+
+static void GLCheckError()
+{
+    while (GLenum error = glGetError())
+    {
+        std::cout << "[OpenGL Error] " << std::hex << error << std::dec << "\n";
+    }
+}
+
+struct ShaderSourceCode
+{
+    std::string vertexShader;
+    std::string fragmentShader;
+};
+
+struct ShaderFilePath
+{
+    std::filesystem::path vertexShader;
+    std::filesystem::path fragmentShader;
+};
+
+static ShaderSourceCode parseShader(const ShaderFilePath& filepaths)
+{
+    std::string line;
+    std::stringstream ss[2];
+
+    std::ifstream stream(filepaths.vertexShader);
+    if (!stream.is_open())
+        std::cerr << "Failed to open vertex shader: " << std::filesystem::absolute(filepaths.vertexShader) << std::endl;
+    while (getline(stream, line))
+    {
+        if (line.find("#shader") == std::string::npos)
+        {
+            ss[0] << line << '\n';
+        }
+    }
+
+    std::ifstream stream2(filepaths.fragmentShader);
+    if (!stream2.is_open())
+        std::cerr << "Failed to open fragment shader: " << std::filesystem::absolute(filepaths.fragmentShader) << std::endl;
+    while (getline(stream2, line))
+        if (line.find("#shader") == std::string::npos)
+        {
+            ss[1] << line << '\n';
+        }
+
+    return {ss[0].str(), ss[1].str() };
+};
+
+static GLuint CompileShader(const std::string& source, GLuint type)
+{
+    std::string typeString;
+    switch (type)
+    {
+        case GL_VERTEX_SHADER:
+            typeString = "vertex";
+            break;
+
+        case GL_FRAGMENT_SHADER:
+            typeString = "fragment";
+            break;
+
+        default:
+            typeString = "unknown";
+    }
+
+    std::cout << "Compiling " << typeString << " shader" << std::endl;
+
+    GLuint id = glCreateShader(type);
+    const char* src = source.c_str();
+    glShaderSource(id, 1, &src, nullptr);
+    glCompileShader(id);
+
+    int result;
+    glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+    if (result == GL_FALSE)
+    {
+        int length;
+        glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+        std::string message;
+        message.resize(length);
+        glGetShaderInfoLog(id, length, &length, message.data());
+
+        std::cout << "failed to compile " << typeString.c_str() << " shader!" << std::endl;
+        std::cout << message << std::endl;
+        glDeleteShader(id);
+        return 0;
+    }
+
+    std::cout << "Successfully compiled " << typeString << " shader" << std::endl;
+
+    return id;
+}
+
+static GLuint CreateShader(const std::string& vertexShader, const std::string& fragmentShader)
+{
+    GLuint program = glCreateProgram();
+    GLuint vs = CompileShader(vertexShader, GL_VERTEX_SHADER);
+    GLuint fs = CompileShader(fragmentShader, GL_FRAGMENT_SHADER);
+
+    glAttachShader(program, vs);
+    glAttachShader(program, fs);
+    glLinkProgram(program);
+    glValidateProgram(program);
+
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+
+    return program;
 }
 
 int main()
@@ -78,68 +166,61 @@ int main()
     gladLoadGL(glfwGetProcAddress);
     glfwSwapInterval(1);
 
+    std::cout << "OpenGL Version: " << glGetString(GL_VERSION) << std::endl;
+
     // NOTE: OpenGL error checks have been omitted for brevity
 
-    GLuint vertex_buffer;
-    glGenBuffers(1, &vertex_buffer);
-    glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    float positions[] = {
+        -0.5f, -0.5f,
+         0.5f,  -0.5f,
+         0.5f,  0.5f,
+        -0.5f,  0.5f
+    };
 
-    const GLuint vertex_shader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertex_shader, 1, &vertex_shader_text, NULL);
-    glCompileShader(vertex_shader);
+    GLuint indices[] = {
+        0, 1, 2,
+        2, 3, 0
+    };
 
-    const GLuint fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragment_shader, 1, &fragment_shader_text, NULL);
-    glCompileShader(fragment_shader);
+    GLuint vao;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
 
-    const GLuint program = glCreateProgram();
-    glAttachShader(program, vertex_shader);
-    glAttachShader(program, fragment_shader);
-    glLinkProgram(program);
+    GLuint bufferID;
+    glGenBuffers(1, &bufferID);
+    glBindBuffer(GL_ARRAY_BUFFER, bufferID);
+    glBufferData(GL_ARRAY_BUFFER, 8 * sizeof(float), positions, GL_STATIC_DRAW);
 
-    const GLint mvp_location = glGetUniformLocation(program, "MVP");
-    const GLint vpos_location = glGetAttribLocation(program, "vPos");
-    const GLint vcol_location = glGetAttribLocation(program, "vCol");
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
+    glEnableVertexAttribArray(0);
 
-    GLuint vertex_array;
-    glGenVertexArrays(1, &vertex_array);
-    glBindVertexArray(vertex_array);
-    glEnableVertexAttribArray(vpos_location);
-    glVertexAttribPointer(vpos_location, 2, GL_FLOAT, GL_FALSE,
-                          sizeof(Vertex), (void*) offsetof(Vertex, pos));
-    glEnableVertexAttribArray(vcol_location);
-    glVertexAttribPointer(vcol_location, 3, GL_FLOAT, GL_FALSE,
-                          sizeof(Vertex), (void*) offsetof(Vertex, col));
+    GLuint ibo;
+    glGenBuffers(1, &ibo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(GLuint), indices, GL_STATIC_DRAW);
+
+    ShaderSourceCode shaders = parseShader(
+        {
+            BASE_PATH / "resources/shaders/basic.vertGL",
+            BASE_PATH / "resources/shaders/basic.fragGL"
+        });
+    unsigned int shader = CreateShader(shaders.vertexShader, shaders.fragmentShader);
+    glUseProgram(shader);
 
     while (!glfwWindowShouldClose(window))
     {
-        int width, height;
-        glfwGetFramebufferSize(window, &width, &height);
-        const float ratio = width / (float) height;
-
-        glViewport(0, 0, width, height);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        mat4x4 m, p, mvp;
-        mat4x4_identity(m);
-        mat4x4_rotate_Z(m, m, (float) glfwGetTime());
-        mat4x4_ortho(p, -ratio, ratio, -1.f, 1.f, 1.f, -1.f);
-        mat4x4_mul(mvp, p, m);
-
-        glUseProgram(program);
-        glUniformMatrix4fv(mvp_location, 1, GL_FALSE, (const GLfloat*) &mvp);
-        glBindVertexArray(vertex_array);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
+
+    glDeleteProgram(shader);
 
     glfwDestroyWindow(window);
 
     glfwTerminate();
     exit(EXIT_SUCCESS);
 }
-
-//! [code]
